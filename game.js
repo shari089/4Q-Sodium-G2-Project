bee5bf97594f30a836600fb7dc5605c8cd76db9a
shari089 <sybtano22@src.pshs.edu.ph>
2025-05-02 01:33:04 +0800

    Update game.js

--- game.js
+++ game.js
@@ -43,6 +43,10 @@ function howToPlay() {
   if (howToPlayOverlay) howToPlayOverlay.style.display = "flex";
 }
 
+function closePopup(popupId) {
+  document.getElementById(popupId).style.display = "none";
+}
+
 function startGame(selectedLevel) {
   level = selectedLevel;
   localStorage.setItem("level", level);
